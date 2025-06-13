ae758c0f8aa424ab5a77108b047b90520e8569ce
Morgane Breton <morgane4478@gmail.com>
2025-06-13 16:57:25 +0200

    màj page d'accueil

--- controllers/confidentiality-policy.php
+++ controllers/confidentiality-policy.php
@@ -5,7 +5,7 @@ require_once '../config/constants.php';
 session_start();
 
 include_once PROJECT_ROOT . '/views/components/header.html';
-include_once PROJECT_ROOT . '/views/mention_legal.html';
+include_once PROJECT_ROOT . '/views/confidentiality-policy.html';
 include_once PROJECT_ROOT . '/views/components/footer.html';
 
 ?> 
--- controllers/legal-notices.php
+++ controllers/legal-notices.php
@@ -5,7 +5,7 @@ require_once '../config/constants.php';
 session_start();
 
 include_once PROJECT_ROOT . '/views/components/header.html';
-include_once PROJECT_ROOT . '/views/politique_confidentialite.html';
+include_once PROJECT_ROOT . '/views/legal-notices.html';
 include_once PROJECT_ROOT . '/views/components/footer.html';
 
 ?> 
