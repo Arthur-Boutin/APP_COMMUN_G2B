695371b4b61af66f0ee5d35fa1f371978f1433f4
alexjavanshir <alexandre.javanshir@gmail.com>
2025-06-13 14:32:44 +0200

    Ajout des CGU et mentions légales

---
+++ controllers/politique_confidentialite.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../config/constants.php';
+
+session_start();
+
+include_once PROJECT_ROOT . '/views/components/header.html';
+include_once PROJECT_ROOT . '/views/politique_confidentialite.html';
+include_once PROJECT_ROOT . '/views/components/footer.html';
+
+?> 
