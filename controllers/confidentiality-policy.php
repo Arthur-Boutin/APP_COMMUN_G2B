64c22c45b1dcf5995a56fe36575b9b0bb0730406
Alexandre Javanshir <61507640+alexjavanshir@users.noreply.github.com>
2025-06-13 19:29:30 +0200

    Alexandre (#6)
    * bouton de connexion
    
    * Changement Main
    
    * Ajout des CGU et mentions légales
    
    * màj page d'accueil
    
    * fix img
    
    ---------
    
    Co-authored-by: Morgane Breton <morgane4478@gmail.com>

---
+++ controllers/confidentiality-policy.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../config/constants.php';
+
+session_start();
+
+include_once PROJECT_ROOT . '/views/components/header.html';
+include_once PROJECT_ROOT . '/views/confidentiality-policy.html';
+include_once PROJECT_ROOT . '/views/components/footer.html';
+
+?> 
